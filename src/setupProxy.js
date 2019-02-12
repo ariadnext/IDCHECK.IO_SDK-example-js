bb37f627fe5760177a09deadeaf557e92cb895df
David Michel <david.michel@ariadnext.com>
2019-02-12 16:31:54 +0100

    refactoring setupProxy: add /api path prefix and error handler

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,16 +1,42 @@
-const { init, start } = require('../middleware');
+const {init, start, getResults, getImage} = require('../middleware');
 
-module.exports = function(app) {
+function handleError(res, error) {
+    res.statusCode = 500;
+    if (error.response && error.response.data) {
+        res.send(error.response.data);
+    } else if (typeof error === 'string') {
+        res.send({errorCode: error, errorMessage: error});
+    } else {
+        res.send(error);
+    }
+}
+
+module.exports = function (app) {
 
     init();
 
-    app.get('/start', (req, res) => {
+    app.get('/api/start', (req, res) => {
         start().then((data) => {
             res.send(data);
         }).catch((error) => {
-            res.statusCode = 500;
-            res.send(error.code);
-            console.error(error);
+            handleError(res, error);
+        });
+    });
+
+    app.get('/api/results/:fileUid', (req, res) => {
+        getResults(req.params.fileUid).then((data) => {
+            res.send(data);
+        }).catch((error) => {
+            handleError(res, error);
+        });
+    });
+
+    app.get('/api/document/:documentUid/image/:imageUid', (req, res) => {
+        getImage(req.params.documentUid, req.params.imageUid).then((data) => {
+            res.write(data);
+            res.end();
+        }).catch((error) => {
+            handleError(res, error);
         });
     });
 
