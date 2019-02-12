ff828ebc12ac18741147d604221bdabe18502f73
David Michel <david.michel@ariadnext.com>
2019-02-12 16:30:52 +0100

    add react-router configuration

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+import Results from './pages/Results';
+import ErrorPage from './pages/Error';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <div>
+            <Route exact path="/" component={Home}/>
+            <Route path="/results" component={Results}/>
+            <Route path="/error" component={ErrorPage}/>
+        </div>
+    </Router>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
