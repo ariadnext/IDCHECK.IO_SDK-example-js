99e546c5c349eb2226969d5b2bd281377abaf40a
David Michel <david.michel@ariadnext.com>
2019-02-12 16:38:23 +0100

    add middleware API to retrieve the analysis results from the CIS API

--- middleware/api/index.js
+++ middleware/api/index.js
@@ -1,58 +1,82 @@
 const axios = require("axios");
 const qs = require('qs');
 
-const sdkWebApi = 'https://desktoptomobile.rennes.ariadnext.com:1443/rest/v1/idcheckio-sdk-web';
-const keycloakUrl = 'https://keycloak.rennes.ariadnext.com:8443';
+class Client {
 
-module.exports = {
-    login: async (username, password, broker) => {
-        return new Promise((resolve, reject) => {
-            axios({
-                method: 'POST',
-                url: `${keycloakUrl}/auth/realms/customer-identity/protocol/openid-connect/token`,
-                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-                data: qs.stringify({
-                    grant_type: 'password',
-                    client_id: 'sdk-web',
-                    username: username,
-                    password: password,
-                    broker: broker,
-                }),
-            }).then(({ data }) => {
-                axios.defaults.headers.common['Authorization'] = `Bearer ${data.access_token}`;
-                resolve(data);
-            }).catch(reject);
+    constructor(sdkWebUrl, keycloakUrl, cisUrl) {
+        this.sdkWebApi = `${sdkWebUrl}/rest/v1/idcheckio-sdk-web`;
+        this.keycloakUrl = keycloakUrl;
+        this.cisApi = `${cisUrl}/rest/v1`;
+    }
+
+    async login(username, password, broker) {
+        const { data } = await axios({
+            method: 'POST',
+            url: `${this.keycloakUrl}/auth/realms/customer-identity/protocol/openid-connect/token`,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            data: qs.stringify({
+                grant_type: 'password',
+                client_id: 'sdk-web',
+                username: username,
+                password: password,
+                broker: broker,
+            }),
         });
-    },
-    getConfiguration: async (code) => {
-        return axios.get(`${sdkWebApi}/configuration/${code}`);
-    },
-    createConfiguration: async (configuration) => {
-        return axios.post(`${sdkWebApi}/configuration`, configuration);
-    },
-    sendLink: async (scenario, confCode, cisRealm, language, contactEmail) => {
-        const timestamp = new Date().getTime();
-        return axios.post(`${sdkWebApi}/onboarding/sendlink`,{
+        axios.defaults.headers.common['Authorization'] = `Bearer ${data.access_token}`;
+        return data;
+    }
+
+    async getConfiguration(code) {
+        return axios.get(`${this.sdkWebApi}/configuration/${code}`);
+    }
+
+    async createConfiguration(configuration) {
+        return axios.post(`${this.sdkWebApi}/configuration`, configuration);
+    }
+
+    async sendLink(scenario, props={}) {
+        const { confCode, cisRealm, fileUid, language, contactEmail, errorRedirectUrl, successRedirectUrl } = props;
+        return axios.post(`${this.sdkWebApi}/onboarding/sendlink`, {
             ...scenario,
-            businessUid: `bus-${timestamp}`,
             interfaceSettings: {
                 ...scenario.interfaceSettings,
                 confCode: confCode,
                 language: language,
             },
-            contactData: contactEmail ? { notificationType: 'EMAIL', value: contactEmail } : null,
+            contactData: contactEmail ? {notificationType: 'EMAIL', value: contactEmail} : null,
             resultHandler: {
                 cisConf: {
                     "realm": cisRealm,
-                    "fileUid": `GithubDemo-${timestamp}`,
-                    "fileLaunchCheck" : true,
-                    "fileCheckWait" : true
+                    "fileUid": fileUid,
+                    "fileLaunchCheck": true,
+                    "fileCheckWait": true
                 }
             },
             options: {
                 "iframeDisplay": !contactEmail,
-                "iframeRedirectParent" : true
+                "iframeRedirectParent": true
             },
+            redirectionData: {
+                "errorRedirectUrl": errorRedirectUrl,
+                "successRedirectUrl": `${successRedirectUrl}?fileUid=${fileUid}`,
+            }
         });
     }
-};
+
+    async getResults(realm, fileUid) {
+        return axios.get(`${this.cisApi}/${realm}/file/${fileUid}`);
+    }
+
+    async getDocument(realm, uid) {
+        return axios.get(`${this.cisApi}/${realm}/document/${uid}`);
+    }
+
+    async getImage(realm, documentUid, uid) {
+        return axios.get(`${this.cisApi}/${realm}/document/${documentUid}/image/${uid}`, {
+            responseType: 'arraybuffer'
+        });
+    }
+
+}
+
+module.exports = Client;
--- middleware/index.js
+++ middleware/index.js
@@ -1,20 +1,14 @@
 const fs = require('fs');
 const path = require('path');
-const { login, getConfiguration, createConfiguration, sendLink } = require('./api');
+const ApiClient = require('./api');
 
 const configurationFolder = 'middleware/configuration';
 const scenariosFolder = 'middleware/scenarios';
-const defaultConfCode = 'githubDemo-Default';
-
-if (!process.env.CONFCODE) {
-    process.env.CONFCODE = defaultConfCode;
-}
-
-if (!process.env.LANGUAGE) {
-    process.env.LANGUAGE = 'EN';
-}
 
 const {
+    SDKWEB_URL: sdkWebUrl,
+    KEYCLOAK_URL: keycloakUrl,
+    CIS_URL: cisUrl,
     CIS_USERNAME: username,
     CIS_PASSWORD: password,
     CIS_REALM: realm,
@@ -22,7 +16,11 @@ const {
     CONTACT_EMAIL: contactEmail,
     SCENARIO: scenarioCode,
     LANGUAGE: language,
+    ERROR_REDIRECT_URL: errorRedirectUrl,
+    SUCCESS_REDIRECT_URL: successRedirectUrl,
 } = process.env;
+
+const client = new ApiClient(sdkWebUrl, keycloakUrl, cisUrl);
 
 function loadConfigurations() {
     const files = fs.readdirSync(path.resolve(configurationFolder));
@@ -33,24 +31,28 @@ function loadConfigurations() {
 }
 
 function getScenario(code) {
-    const file = fs.readFileSync(path.resolve(`${scenariosFolder}/${code}.json`));
-    return JSON.parse(file);
+    try {
+        const file = fs.readFileSync(path.resolve(`${scenariosFolder}/${code}.json`));
+        return JSON.parse(file);
+    }catch(error){
+        throw `Cannot find scenario ${code}.`;
+    }
 }
 
 module.exports = {
 
     init: async () => {
         console.log('initialization...');
-        await login(username, password, realm);
+        await client.login(username, password, realm);
         try {
-            await getConfiguration(confCode);
+            await client.getConfiguration(confCode);
         }catch(error){
-            if (error.response.status && error.response.status === 404) {
+            if (error.response && error.response.status && error.response.status === 404) {
                 const configurations = loadConfigurations();
                 const configuration = configurations.find(c => c.code === confCode);
                 if (configuration) {
                     console.log(`creating configuration ${confCode}...`);
-                    await createConfiguration(configuration);
+                    await client.createConfiguration(configuration);
                 }else{
                     throw `Cannot find configuration with code '${confCode}' in directory ${configurationFolder}.`;
                 }
@@ -61,10 +63,37 @@ module.exports = {
     },
 
     start: async () => {
-        await login(username, password, realm);
+        await client.login(username, password, realm);
         const scenario = getScenario(scenarioCode);
-        const { data } = await sendLink(scenario, confCode, realm, language, contactEmail);
-        return { ...data, email: !!contactEmail };
+        const fileUid = `GithubDemo-${new Date().getTime()}`;
+        const { data } = await client.sendLink(scenario, {
+            confCode,
+            cisRealm: realm,
+            fileUid,
+            language,
+            contactEmail,
+            errorRedirectUrl,
+            successRedirectUrl,
+        });
+        return { ...data, fileUid, email: !!contactEmail };
+    },
+
+    getResults: async (fileUid) => {
+        await client.login(username, password, realm);
+        const { data: reportData } = await client.getResults(realm, fileUid);
+        const documents = await Promise.all(reportData.documents.map(doc => {
+            return client.getDocument(realm, doc.uid);
+        }));
+        return {
+            report: reportData.lastReport,
+            documents: documents.map(doc => doc.data),
+        };
+    },
+
+    getImage: async (documentUid, imageUid) => {
+        await client.login(username, password, realm);
+        const { data } = await client.getImage(realm, documentUid, imageUid);
+        return Buffer.from(data, 'binary');
     }
 
 };
